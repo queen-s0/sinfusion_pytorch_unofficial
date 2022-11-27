ab45d4db600d5352edd059b8454b1ad3ea9ed607
Ruslan Gabdullin <rgabdullin@cs.msu.ru>
2022-11-27 19:14:25 +0000

    Docker

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 set -e
 
 CMD="jupyter notebook --ip=0.0.0.0 --port=7777 --allow-root --NotebookApp.token='mytoken' --NotebookApp.password=''"
-docker build -t sinfusion_pytorch_unofficial .
-docker run --rm -it sinfusion_pytorch_unofficial -v $(pwd):/workspace -c "$CMD"
+docker build -t rgabdullin/sinfusion_pytorch_unofficial:dev .
+docker push rgabdullin/sinfusion_pytorch_unofficial:dev
+# docker run --rm -it -v $(pwd):/workspace -p 7777:7777 sinfusion_pytorch_unofficial -c "$CMD"
