e973b802208c47d98b0b42ed015a3885915f0ade
Ruslan Gabdullin <rgabdullin@cs.msu.ru>
2022-11-28 22:50:42 +0300

    WIP

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--image_path', type=str, required=True)
     parser.add_argument('-T', '--timesteps', type=int, default=50)
     parser.add_argument('-l', '--loss_type', type=str, default='l2')
-    parser.add_argument('--sampling_timesteps', type=int, default=50)
+    parser.add_argument('--sampling_timesteps', type=int, default=512)
     parser.add_argument('--image_size', type=int, default=128)
     args = parser.parse_args()
     
